0ff30741b7d6c3386194b357e4ca1823cbff095c
Zdenek ULRYCH <ulrychz@office365.zcu.cz>
2024-05-09 16:17:20 +0200

    cv 12

--- Ct1450_SVGAnimace/Models/Tvar.cs
+++ Ct1450_SVGAnimace/Models/Tvar.cs
@@ -16,5 +16,13 @@ namespace Ct1450_SVGAnimace.Models
         public int PozY { get; set;}
         public Color Barva { get; set; }
         public TypTvaru TypTvaru { get; set; }
+
+        public void Posun(int krok, int svgWidth, int svgHeight)
+        {
+            PozX += krok;
+            PozY += krok;
+            if (PozX >= svgWidth) { PozX = 0; }
+            if (PozY >= svgHeight) { PozY = 0; }
+        }
     }
 }
--- Ct1450_SVGAnimace/Pages/SVG.razor.cs
+++ Ct1450_SVGAnimace/Pages/SVG.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Ct1450_SVGAnimace.Models;
+using Newtonsoft.Json;
 using System.Drawing;
 
 namespace Ct1450_SVGAnimace.Pages
@@ -11,10 +12,16 @@ namespace Ct1450_SVGAnimace.Pages
             SvgHeight = 800;
         }
 
+        protected override async Task OnInitializedAsync()
+        {
+            await Nahrat();
+        }
         public int SvgWidth { get; set; }
         public int SvgHeight { get; set;}
         public int MinRozmer { get; set; } = 20;
         public int MaxRozmer { get; set; } = 80;
+        public int KrokAnimace { get; set; } = 1;
+        public bool AnimaceBezi { get; set; } = false;
         public List<Models.Tvar> SvgTvaryList { get; set; } = new List<Models.Tvar>();
 
         private Random rnd = new Random();
@@ -42,6 +49,57 @@ namespace Ct1450_SVGAnimace.Pages
             if(tvar != null)
                 SvgTvaryList.Add(tvar);
 
+        }
+        private void OdebratTvar()
+        {
+            if (SvgTvaryList.Any())
+            {
+                SvgTvaryList.RemoveAt(SvgTvaryList.Count - 1);
+            }
+        }
+
+        private async Task Ulozit()
+        {
+            string json = JsonConvert.SerializeObject(SvgTvaryList, 
+                new JsonSerializerSettings {TypeNameHandling = TypeNameHandling.Auto });
+
+            await localStorage.SetItemAsync("dataTvary", json);
+        }
+
+        private async Task Nahrat()
+        {
+            var json = await localStorage.GetItemAsync<string>("dataTvary");
+            if (json != null)
+            {
+                List<Models.Tvar>? products = JsonConvert.DeserializeObject<List<Models.Tvar>>(json, 
+                    new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto });
+                if (products != null) 
+                    SvgTvaryList.AddRange(products);
+            }
+
+        }
+        private async Task Smazat(Microsoft.AspNetCore.Components.Web.MouseEventArgs e)
+        {
+            await localStorage.RemoveItemAsync("dataTvary");
+        }
+
+        private async Task PosunTvaru()
+        {
+            AnimaceBezi = true;
+            do 
+            {
+                foreach (var item in SvgTvaryList)
+                {
+                    item.Posun(KrokAnimace, SvgWidth, SvgHeight);
+                }
+                StateHasChanged();
+                await Task.Delay(10);
+            } while (AnimaceBezi);
+        }
+
+        private void PosunZastavit()
+        {
+            AnimaceBezi = false;
         }
     }
 }
